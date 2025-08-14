3fcd48e7a82333716e1710d258386ad03d6a90d5
myron <myrondabreo@protonmail.com>
2025-08-14 20:41:38 +0530

    day_2 complete

--- Day_2/Day_2.py
+++ Day_2/Day_2.py
@@ -1,58 +1,58 @@
 # Q1]1. Check if a number is positive, negative, or zero
 
-# while True:
-#     num=int(input("Enter Number:"))
-#     if num==0:
-#         print(num)
-#     elif num<0:
-#         print("Negative")
-#     elif num>0:
-#         print("Positive")
+while True:
+    num=int(input("Enter Number:"))
+    if num==0:
+        print(num)
+    elif num<0:
+        print("Negative")
+    elif num>0:
+        print("Positive")
 
-#OR Better version
+# OR Better version
 
-# while True:
-#     user_ip=input("Enter value:")
+while True:
+    user_ip=input("Enter value:")
 
-#     if user_ip.lower()=="quit":
-#         print("Exit")
-#         break
+    if user_ip.lower()=="quit":
+        print("Exit")
+        break
 
-# #lstrip because isdidgits does not classify negative numbers so it we input -1 it will be invalid
-#     elif not user_ip.lstrip("-").isdigit():
-#         print("Invalid")
-#         continue
+#lstrip because isdidgits does not classify negative numbers so it we input -1 it will be invalid
+    elif not user_ip.lstrip("-").isdigit():
+        print("Invalid")
+        continue
 
-#     nums=int(user_ip)
+    nums=int(user_ip)
 
-#     if nums==0:
-#         print("Zero")
-#     elif nums<0:
-#         print("Negative")
-#     elif nums>0:
-#         print("Positive")
+    if nums==0:
+        print("Zero")
+    elif nums<0:
+        print("Negative")
+    elif nums>0:
+        print("Positive")
 
 
 # Q2]Print first 10 even numbers using for loop.
 
-# lst=list(range(1,21))
-# count=0
+lst=list(range(1,21))
+count=0
 
-# for value in lst:
-#     if value%2==0:
-#         count+=1
-#         print(count,value)
+for value in lst:
+    if value%2==0:
+        count+=1
+        print(count,value)
 
 # Q3]Use a while loop to reverse digits of a number.
 
-# num=int(input("Enter Number:"))
-# rev=0
+num=int(input("Enter Number:"))
+rev=0
 
-# while num>0:
-#     digit=num%10
-#     rev=rev*10+digit
-#     num//=10
-# print(rev)
+while num>0:
+    digit=num%10
+    rev=rev*10+digit
+    num//=10
+print(rev)
 
 # Q4] Palindrome Number Check
 # Ask the user for a number, reverse it using the above logic, and check if it’s the same as the original.
@@ -60,64 +60,64 @@
 # Input: 121 → Output: Palindrome
 # Input: 123 → Output: Not Palindrome
 
-# num=int(input("Enter Number:"))
-# rev=0
-# stored=num
+num=int(input("Enter Number:"))
+rev=0
+stored=num
 
-# while num>0:
-#     digit=num%10
-#     rev=rev*10+digit
-#     num//=10
+while num>0:
+    digit=num%10
+    rev=rev*10+digit
+    num//=10
 
 
-# if rev==stored:
-#     print("Palindrome")
+if rev==stored:
+    print("Palindrome")
 
-# else:
-#     print("NOT")
+else:
+    print("NOT")
 
 # Q5] Reverse Until Negative
 # Keep asking the user for a number and reverse it using the while loop.
 # If the user enters a negative number, stop the program.
 
-# while True:
-#     user_input=input("Enter number:")
+while True:
+    user_input=input("Enter number:")
 
-#     if user_input.lower=="quit":
-#         print("Exit")
-#         break
+    if user_input.lower=="quit":
+        print("Exit")
+        break
 
-#     elif not user_input.lstrip("-").isdigit():
-#         print("Invalid")
-#         continue
+    elif not user_input.lstrip("-").isdigit():
+        print("Invalid")
+        continue
 
-#     num=int(user_input)
-#     rev=0
-#     if num<0:
-#         print("First Negative Number")
-#         break
+    num=int(user_input)
+    rev=0
+    if num<0:
+        print("First Negative Number")
+        break
 
-#     while num>0:
-#         digit=num%10
-#         rev=rev*10+digit
-#         num//=10
-#     print(rev)
+    while num>0:
+        digit=num%10
+        rev=rev*10+digit
+        num//=10
+    print(rev)
 
 #Q6] Reverse Without Losing Leading Zeros
 # Example: 1050 → 0501 (but as a string, because integers drop leading zeros).    
 
-# user_input=input("Enter Value:")
+user_input=input("Enter Value:")
 
-# if not user_input.isdigit():
-#     print("Invalid")
+if not user_input.isdigit():
+    print("Invalid")
 
-# rev=reversed(user_input)
-# lst=list(rev)
+rev=reversed(user_input)
+lst=list(rev)
 
-# print("".join(lst))
+print("".join(lst))
 
 
-# Q]Use match-case to print grade from letter input.
+# Q7]Use match-case to print grade from letter input.
 
 grade=input("Enter Grade:")
 grade=grade.upper()
@@ -138,4 +138,17 @@ match grade:
     case _:
         print("Invalid Grade")
 
-# Print Fibonacci sequence up to n terms.
+# Q8] Print Fibonacci sequence up to n terms.
+
+n=int(input("Enter a value:"))
+
+lst=list(range(n+1))
+print(lst)
+i=0
+sum=0
+
+while i <len(lst):
+    sum=sum+lst[i]
+    i+=1
+
+print(sum)
